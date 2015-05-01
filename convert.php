22174559e0dcf15385d9a093c6edf601c02246e1
Ryan Pope <poperyan73@gmail.com>
2015-05-01 08:32:48 -0500

    Restructure, delete stuff

--- convert.php
+++ convert.php
@@ -2,57 +2,22 @@
 
 $json = 
 '{
-    {
-        "tag": "div",
+    "div#world": {
         "attr": {
-            "class": "container"
+            "class": "whatever"
         },
-        "value": {
-            "tag": "p",
-            "attr": {
-                "class": "text",
-                "id": "hello-world"
-            },
-            "value": "Hello World!"
-        }
+        "parent": "none"
+    },
+    "div#stuff": {
+        "attr": {
+            "class": "text"
+        },
+        "parent": "div#world"
     }
 }';
 
-/*$json = $_POST["input"];*/
 $data = json_decode($json, true);
-var_dump($data);
-function iterate($stuff) {
-    foreach ($stuff as $object) {
-        foreach ($object as $key => $value) {
-            $tag = $getTag($key);
-            $attrs = getAttrs($tag);
-            $tagValue = getValue($key);
-            ?> <<?=$tag?> <?php
-            foreach ($attrs as $attr => $var) {
-                ?> <?=$attr?>="<?=$var?>" <?php
-            }
-            ?> ><?=$value?> </<?=$tag?>> <?php
-        }
-    }
-}
 
-function getAttrs($key) {
-    if ($key == "attr") {
-        return $key;
-    }
-    return false;
-}
-
-function getTag($key) {
-    if ($key == "tag") {
-        return $key;
-    }
-    return false;
-}
-
-function getValue($key) {
-    if ($key == "value") {
-        return $key;
-    }
-    return false;
+foreach ($data as $tag => $stuff) {
+    
 }
