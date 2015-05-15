97e7d14927e2ba77dad7a1a044005ee8f13a0db1
Ryan Pope <poperyan73@gmail.com>
2015-05-15 08:34:26 -0500

    Made it work

--- convert.php
+++ convert.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once "Tag.php";
+
 $json = 
 '{
     "div#world": {
@@ -12,12 +14,26 @@ $json =
         "attr": {
             "class": "text"
         },
-        "parent": "div#world"
+        "parent": "div#world",
+        "value": "Hello World!"
     }
 }';
 
 $data = json_decode($json, true);
 
 foreach ($data as $tag => $stuff) {
-    
+    if (strpos('#', $tag) != false)
+    {
+        $tagId = explode("#", $tag);
+        $tagName = $tagId[0];
+        $id = $tagId[1];
+    }
+    else
+    {
+        $tagName = "div";
+        $id = null;
+    }
+    $value = isset($stuff["value"]) ? $stuff["value"] : null;
+    $newTag = new Tag($tagName, $id, $stuff["attr"], $stuff["parent"], $value);
+    $newTag->printTag();
 }
